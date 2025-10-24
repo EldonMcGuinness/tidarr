27ac041be43fdfaf48ccc1f8460e912bc991353a
Eldon McGuinness <eldon.mcguinness@gmail.com>
2025-10-24 18:37:46 -0400

    Added Proper umask support and removed chmod.

--- api/src/helpers/jobs.ts
+++ api/src/helpers/jobs.ts
@@ -119,19 +119,8 @@ export function replacePathInM3U(): void {
   }
 }
 
-function setUmask(umask: string) {
-  const output_chmod = execSync(
-    `chmod -R ${umask} ${ROOT_PATH}/download/incomplete/*`,
-    {
-      encoding: "utf-8",
-    },
-  );
-  console.log(`- Chmod: ${umask}`, output_chmod);
-}
-
 export async function setPermissions() {
   if (process.env.PUID && process.env.PGID) {
-    setUmask(process.env.UMASK || "755");
 
     const output_chown = execSync(
       `chown -R ${process.env.PUID}:${process.env.PGID} ${ROOT_PATH}/download/incomplete/*`,
@@ -146,7 +135,4 @@ export async function setPermissions() {
     return;
   }
 
-  if (process.env.UMASK) {
-    setUmask(process.env.UMASK);
-  }
 }
--- docker/run-prod.sh
+++ docker/run-prod.sh
@@ -1,5 +1,11 @@
 # AUTH JWT RANDOM SECRET
 export JWT_SECRET=`tr -dc A-Za-z0-9 </dev/urandom | head -c 15; echo`
 
+# Check if UMASK is set, otherwise use a default value
+UMASK_VALUE=${UMASK:-0022}
+
+# Apply the umask
+umask "$UMASK_VALUE"
+
 sleep 2
 yarn --cwd ./api prod
